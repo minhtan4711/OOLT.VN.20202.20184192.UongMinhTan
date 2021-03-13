01081ba9746e6c8c051b38fa3e98e2b6c5a6f0c5
uongminhtan <minhtan471100@gmail.com>
2021-03-13 23:11:07 +0700

    update

---
+++ Lab01/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        int arr[] = new int [8];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
